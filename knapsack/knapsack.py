3f6c74f14c08c4054a0760639c82a1902db852b5
John Rojcewicz <rojcewiczj@gmail.com>
2020-01-23 18:29:49 -0600

    finsihed

--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -4,11 +4,52 @@ import sys
 from collections import namedtuple
 
 Item = namedtuple('Item', ['index', 'size', 'value'])
+file_contents = open('c:/Users/rojce/documents/GitHub/Algorithms/knapsack/data/small3.txt', 'r')
+small_1_items = []
+    
+for line in file_contents.readlines():
+    data = line.rstrip().split()
+    small_1_items.append(Item(int(data[0]), int(data[1]), int(data[2])))
+
+    file_contents.close()
+
 
 def knapsack_solver(items, capacity):
-  pass
-  
+  bag= []
+  total_cost = 0
+  value_index = 0
+  total_value = 0 
+  values=[]
+  maybe_bag={}
+  size_bag={}
+  for item in items:
+     maybe_bag[item.value/item.size] = [item.index, item.size, item.value]
+     values.append(item.value/item.size)
+  values = sorted(values, reverse = True)
 
+  for value in values:
+  
+      if total_cost + maybe_bag[value][1] > capacity:
+         break
+      else:
+        total_cost = total_cost + maybe_bag[value][1]
+        total_value += maybe_bag[value][2]
+        bag.append(int(maybe_bag[value][0]))
+  print(total_value)
+  print(sorted(bag))
+  return{'Value': int(total_value), 'Chosen': sorted(bag)}
+  
+  # for value in values:
+  #   print(maybe_bag[value][0])
+    
+  # for item in values:
+  #   if capacity == 0:
+  #      bag.append({'Value': totalValue , 'Chosen': chosenIndex})
+  #      return bag
+    
+ 
+
+print(knapsack_solver(small_1_items, 100))
 if __name__ == '__main__':
   if len(sys.argv) > 1:
     capacity = int(sys.argv[2])
--- making_change/making_change.py
+++ making_change/making_change.py
@@ -3,7 +3,19 @@
 import sys
 
 def making_change(amount, denominations):
-  pass 
+  my_array = [0]*(amount+1)
+  if amount == 0:
+    return 1
+  else:
+    for coin in denominations:
+        for i in range(amount+1):
+            if coin == i:
+                my_array[i]+=1
+            if i-coin>0:
+                my_array[i]=((my_array)[i]+my_array[i-coin])
+  return(my_array[-1])
+
+print(making_change(0, [1, 5, 10, 25, 50]))
 
 
 if __name__ == "__main__":
